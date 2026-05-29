f319b4db878ddc894d312f38e287fc83174c0ea1
Stefan Wohlgensinger <stefan.wohlgensinger@gmail.com>
2026-05-29 18:38:18 +0200

    perf: implemented high-speed deterministic ascii character detector

--- benchmark.py
+++ benchmark.py
@@ -1,5 +1,6 @@
-from src.detector import detect_frequent_characters
+from src.detector import detect_frequent_ascii_characters, detect_frequent_characters
 
 if __name__ == "__main__":
     test_input = ("x" + "y") * 50000  # 100'000 characters
     detect_frequent_characters(test_input)
+    detect_frequent_ascii_characters(test_input)
--- src/detector.py
+++ src/detector.py
@@ -1,4 +1,7 @@
+from typing import List
+
 from line_profiler import profile
+import array
 
 @profile
 def detect_frequent_characters(string: str | list | tuple):
@@ -16,6 +19,23 @@ def detect_frequent_characters(string: str | list | tuple):
    # print(found_once)
     return found_frequently
 
+@profile
+def detect_frequent_ascii_characters(string: str | list | tuple):
+    ascii_tracker = array.array('L', [0] * 256)
+    
+    try:
+        for e in string:
+            ascii_tracker[ord(e)] += 1
+    except IndexError:
+        raise ValueError(f"Character {e} lies outside the legal ascii character code range.")
+
+    found_frequently: List[str] = list()
+    for index, value in enumerate(ascii_tracker):
+        if value >= 2:
+            found_frequently.append(chr(index))
+    return found_frequently
+
 if __name__ == "__main__":
     print("Running detector in terminal:")
     detect_frequent_characters("banana")
+    detect_frequent_ascii_characters("banana")
