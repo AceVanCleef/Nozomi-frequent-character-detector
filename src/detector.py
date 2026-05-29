33e7305028fbfd17f623cac2a588717b15013d28
Stefan Wohlgensinger <stefan.wohlgensinger@gmail.com>
2026-05-29 16:16:46 +0200

    feat: implemented duplicate character detection using single-pass dual sets

--- src/detector.py
+++ src/detector.py
@@ -1,6 +1,21 @@
 from line_profiler import profile
 
-
 @profile
-def detect_frequent_characters(string: str):
-    return []
+def detect_frequent_characters(string: str | list | tuple):
+    found_once: set[str] = set()
+    found_frequently: set[str] = set()
+    for e in string:
+        if e in found_frequently:
+            next
+        else:
+            if e in found_once:
+                found_frequently.add(e)
+            else:
+                found_once.add(e)
+   # print(found_frequently)
+   # print(found_once)
+    return found_frequently
+
+if __name__ == "__main__":
+    print("Running detector in terminal:")
+    detect_frequent_characters("banana")
